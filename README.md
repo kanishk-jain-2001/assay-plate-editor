215cc5004a19c55151e4e61e303245945ded8c75
Kanishk Jain <kanishkjain@Kanishks-MacBook-Pro.local>
2024-03-06 14:26:35 -0500

    Initialized frontend and backend folders

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # assay-plate-editor
 Creating an Interactive Assay Plate Editor and Viewer Application 
+
+## Backend 
+The backend will have a few API's 
---
+++ backend/app.py
@@ -0,0 +1,3 @@
+# This will be a flask app that includes all of the endpoints for the project:
+
+# We will also have to connect to a database, so lets use postgresql. 
