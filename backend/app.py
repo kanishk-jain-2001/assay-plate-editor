b45674dbcada204e346f34f6839d4ed229641f9c
Kanishk Jain <kanishkjain@Kanishks-MacBook-Pro.local>
2024-03-06 17:25:07 -0500

    Saving Changes

--- backend/app.py
+++ backend/app.py
@@ -3,38 +3,47 @@ import models
 
 app = Flask(__name__)
 
-# with app.app_context():
-#     models.db.create_all()
+app.config["SQLALCHEMY_DATABASE_URI"] =  "postgresql://postgres:password@127.0.0.1:5432/bio-assay-app"
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "127.0.0.1:5432"
 models.db.init_app(app)
+
+with app.app_context():
+    models.db.create_all()
 
 @app.route("/view-assay-plates")
 def view_plates():
+
     """
     Returns a list of all configured plates in the database 
     """
+
     return "<p>Hello, World!</p>"
 
 @app.route("/delete-assay-plate")
 def delete_plate():
+
     """
     Deletes a specific plate from the database. 
     """
+
     return "<p>Hello, World!</p>"
 
 @app.route("/create-assay-plate")
 def create_plate():
+
     """
     Creates an Assay Plate 
     """
+
     return "<p>Hello, World!</p>"
 
 @app.route("/update-or-add-assay-plate-info")
 def update_add_plate_info():
+
     """
     Allows the user to update or add an assay plate of their choosing 
     """
+
     return "<p>Hello, World!</p>"
 
 
--- backend/models.py
+++ backend/models.py
@@ -1,5 +1,7 @@
-from sqlalchemy.orm import DeclarativeBase
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import String
+from sqlalchemy.orm import mapped_column, Mapped, DeclarativeBase
+from typing import Optional
 
 class Base(DeclarativeBase):
   pass
@@ -9,8 +11,19 @@ db = SQLAlchemy(model_class=Base)
 # Table 1 Contains all the master data for a Table
 class Plates(Base):
     __tablename__ = "plates"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str]
+    fullname: Mapped[str] = mapped_column(String(30))
+    nickname: Mapped[Optional[str]]
+
 
 # Table 2 Contains the detailed information for the 96 well plates, foreign key is connected to table 1 
+class Plate96(Base):
+   __tablename__ = "96_well_plates"
+
 
 # Table 3 contains the detailed informatino for the 136 well plates, foreign key is connected to table 1
+class Plate384(Base):
+   __tablename__ = "384_well_plates"
+
 
