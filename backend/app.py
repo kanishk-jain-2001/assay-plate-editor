e0ca51a784ba88ccb2e9eec4a3158255372c0db9
Kanishk Jain <kanishkjain@Kanishks-MacBook-Pro.local>
2024-03-07 09:58:21 -0500

    Completed Update/Add Endpoint

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,9 @@
-from flask import Flask, jsonify 
+from flask import Flask, request, jsonify 
+from flask_cors import CORS
 import models 
 
 app = Flask(__name__)
+CORS(app)
 
 app.config["SQLALCHEMY_DATABASE_URI"] =  "postgresql://postgres:password@127.0.0.1:5432/bio-assay-app"
 
@@ -20,19 +22,48 @@ def view_plates():
 def delete_plate():
     """ Deletes a specific plate from the database """
 
-    return "<p>Hello, World!</p>"
+    return jsonify("Table has been deleted successfully")
 
-@app.route("/create-assay-plate")
-def create_plate():
-    """ Creates an Assay Plate  """
-
-    return jsonify("Table has been made successfully")
-
-@app.route("/update-or-add-assay-plate-info")
+@app.route("/update-or-add-assay-plate", methods=['POST'])
 def update_add_plate_info():
-    """ Allows the user to update or add an assay plate of their choosing """
+    """ Allows the user to update or add either a 96 or a 384 well plate"""
+    data = request.json
+    plate_id = data.get('id')
+
+    # Logic for updating assay plate information
+    if plate_id: 
+        plate = models.Plates.query.get(plate_id)
+        if not plate: 
+            return jsonify({"error": "Plate not found"}), 404
+        if plate.type == "96":
+            plate96 = models.Plate96.query.filter_by(plate_id=plate_id).first()
+            if plate96:
+                plate96.wells = data['wells']
+            else:
+                return jsonify({"error": "Plate96 details not found"}), 404
+        elif plate.type == "384":
+            plate384 = models.Plate384.query.filter_by(plate_id=plate_id).first()
+            if plate384:
+                plate384.wells = data['wells']
+            else: 
+                return jsonify({"error":"Plate384 details not found"}), 404
     
-    return "<p>Hello, World!</p>"
+    # Logic for adding a new assay plate
+    else: 
+        new_plate = models.Plates(type=data['type'])
+        models.db.session.add(new_plate)
+        models.db.session.flush()
+
+        if data['type'] == "96": 
+            new_plate96 = models.Plate96(plate_id=new_plate.id, wells=data['wells'])
+            models.db.session.add(new_plate96)
+        elif data['type'] == "384": 
+            new_plate384 = models.Plate384(plate_id=new_plate.id, wells=data['wells'])
+            models.db.session.add(new_plate384)
+
+    models.db.session.commit()
+    return jsonify({"message":"Plate processed successfuly"}), 200
+
 
 if __name__ == '__main__':
     app.run()
--- backend/models.py
+++ backend/models.py
@@ -1,5 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, ARRAY, ForeignKey
+from sqlalchemy import JSON, ForeignKey 
 from sqlalchemy.orm import mapped_column, Mapped, DeclarativeBase
 
 class Base(DeclarativeBase):
@@ -10,19 +10,19 @@ db = SQLAlchemy(model_class=Base)
 # Table 1 Contains all the master data for a Table
 class Plates(Base):
     __tablename__ = "plates_master"
-    id: Mapped[int] = mapped_column(primary_key=True)
-    type: Mapped[str] = mapped_column(String)
+    id: Mapped[int] = mapped_column(primary_key = True)
+    type: Mapped[int] = mapped_column(nullable = False)
 
 # Table 2 Contains the detailed information for the 96 well plates, foreign key is connected to table 1 
 class Plate96(Base):
    __tablename__ = "96_well_plates"
-   id: Mapped[int] = mapped_column(primary_key=True)
-   plate: Mapped[int] = mapped_column(ForeignKey("plates_master.id"))
-   A: Mapped[list] = mapped_column(ARRAY(int))
+   id: Mapped[int] = mapped_column(primary_key = True)
+   plate_id: Mapped[int] = mapped_column(ForeignKey("plates_master.id"), nullable = False)
+   wells: Mapped[dict] = mapped_column(type_=JSON)
 
 # Table 3 contains the detailed information for the 136 well plates, foreign key is connected to table 1
 class Plate384(Base):
    __tablename__ = "384_well_plates"
    id: Mapped[int] = mapped_column(primary_key=True)
-   plate: Mapped[int] = mapped_column(ForeignKey("plates_master.id"))
-   A: Mapped[list] = mapped_column(ARRAY(int))
+   plate_id: Mapped[int] = mapped_column(ForeignKey("plates_master.id"), nullable = False)
+   wells: Mapped[dict] = mapped_column(type_=JSON)
