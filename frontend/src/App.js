9c69a74a0b84496e559feaf394f936d5fc3035a6
Kanishk Jain <kanishkjain@Kanishks-MacBook-Pro.local>
2024-03-07 16:16:26 -0500

    Frontend edits

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,16 @@
-import './App.css';
+import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 export default function App () {
   return (
     <>
-      <div className="App">
-        Hello
-      </div>
+      <Container className='text-center align-middle'>
+        <div> Assay Plate Editor Application </div>
+        <Button variant="primary"> Add a 96 Well Plate </Button>
+        <Button variant="primary"> Add a 384 Well Plate </Button>
+     </Container>
     </>
   )
 };
